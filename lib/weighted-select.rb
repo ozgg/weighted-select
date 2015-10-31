81b623e3c990eddc0d600f8fea41b12bf9c0c7cd
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-11-01 01:20:30 +0300

    Added comments for metods

--- lib/weighted-select.rb
+++ lib/weighted-select.rb
@@ -4,6 +4,11 @@ module WeightedSelect
   class Selector
     attr_reader :weights, :total_weight
 
+    # Sets initial weights
+    #
+    # Accepts Hash as an argument, where keys are items and corresponding values
+    # are their weights (positive integers).
+    #
     # @param [Hash] weights
     def initialize(weights = Hash.new)
       @weights = Hash.new
@@ -11,6 +16,10 @@ module WeightedSelect
       weights.each { |item, weight| add item, weight }
     end
 
+    # Add item with weight
+    #
+    # @param [Object] item
+    # @param [Integer] weight
     def add(item, weight)
       delta = Integer(weight)
       if delta > 0
@@ -20,12 +29,18 @@ module WeightedSelect
       end
     end
 
+    # Select weighted-random item if where are any in weights hash
+    #
+    # @return [Object|nil]
     def select
       extract_item unless @weights.empty?
     end
 
     private
 
+    # Extract item based on weights distribution
+    #
+    # @return [Object]
     def extract_item
       weight = Random.rand(@total_weight)
       @weights.each do |range, item|
